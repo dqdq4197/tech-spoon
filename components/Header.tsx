cc1a9201a3d62660dec24a64c4d9b3d496cccc68
choiheesu <dqdq4197@naver.com>
2025-06-29 15:21:29 +0900

    chore: update site metadata

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,18 +15,13 @@ const Header = () => {
   return (
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
-          <div className="mr-3">
-            <Logo />
+        {typeof siteMetadata.headerTitle === 'string' ? (
+          <div className="hidden h-6 text-2xl font-semibold sm:block">
+            {siteMetadata.headerTitle}
           </div>
-          {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
-          ) : (
-            siteMetadata.headerTitle
-          )}
-        </div>
+        ) : (
+          siteMetadata.headerTitle
+        )}
       </Link>
       <div className="flex items-center space-x-4 leading-5 sm:-mr-6 sm:space-x-6">
         <div className="no-scrollbar hidden max-w-40 items-center gap-x-4 overflow-x-auto sm:flex md:max-w-72 lg:max-w-96">
