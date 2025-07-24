db04d1d097fd6c7c71b49c5b08f26d3edc65fb84
choiheesu <dqdq4197@naver.com>
2025-07-24 12:15:12 +0900

    feat: not-found 페이지에 SectionContainer 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,25 +1,28 @@
 import Link from '@/components/Link'
+import SectionContainer from '@/components/SectionContainer'
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
-      <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-        <h1 className="text-6xl leading-9 font-extrabold tracking-tight text-gray-900 md:border-r-2 md:px-6 md:text-8xl md:leading-14 dark:text-gray-100">
-          404
-        </h1>
+    <SectionContainer>
+      <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
+        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-6xl leading-9 font-extrabold tracking-tight text-gray-900 md:border-r-2 md:px-6 md:text-8xl md:leading-14 dark:text-gray-100">
+            404
+          </h1>
+        </div>
+        <div className="max-w-md">
+          <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">
+            죄송해요, 요청하신 페이지를 찾을 수 없어요.
+          </p>
+          <p className="mb-8">하지만 괜찮아요! 홈페이지에서 다양한 콘텐츠를 만나보실 수 있어요.</p>
+          <Link
+            href="/"
+            className="focus:shadow-outline-blue bg-brand-100 hover:bg-brand-105 inline rounded-lg border border-transparent px-4 py-2 text-sm leading-5 font-medium text-white shadow-xs transition-colors duration-150 focus:outline-hidden"
+          >
+            홈페이지로 돌아가기
+          </Link>
+        </div>
       </div>
-      <div className="max-w-md">
-        <p className="mb-4 text-xl leading-normal font-bold md:text-2xl">
-          죄송해요, 요청하신 페이지를 찾을 수 없어요.
-        </p>
-        <p className="mb-8">하지만 괜찮아요! 홈페이지에서 다양한 콘텐츠를 만나보실 수 있어요.</p>
-        <Link
-          href="/"
-          className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm leading-5 font-medium text-white shadow-xs transition-colors duration-150 hover:bg-blue-700 focus:outline-hidden dark:hover:bg-blue-500"
-        >
-          홈페이지로 돌아가기
-        </Link>
-      </div>
-    </div>
+    </SectionContainer>
   )
 }
