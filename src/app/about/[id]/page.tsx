9df204a93d3446871df79047c96917d4e4dfe5f5
choiheesu <dqdq4197@naver.com>
2025-08-29 19:27:18 +0900

    feat: about 페이지에 author id 동적 세그먼트 페이지 추가

--- src/app/about/[id]/page.tsx
+++ src/app/about/[id]/page.tsx
@@ -3,11 +3,18 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from '@/app/seo'
 import { resolveAuthors } from '@/utils'
+import type { Author } from 'contentlayer/generated'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
-function Page() {
-  const author = resolveAuthors(['heesu'])[0]
+interface Props {
+  params: Promise<{ id: Author['id'] }>
+}
+
+async function Page(props: Props) {
+  const { params } = props
+  const { id } = await params
+  const author = resolveAuthors([id])[0]
   const mainContent = coreContent(author)
 
   return (
